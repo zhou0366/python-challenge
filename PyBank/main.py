e37ec69df34b43e7024adf46dd36b1e6489006af
zhou0366 <zhou0366@umn.edu>
2024-01-15 20:41:11 -0600

    initial code

---
+++ PyBank/main.py
@@ -0,0 +1,65 @@
+#starting off initializing and reading in the CSV file using code from activity 3-8
+#initialize
+from ast import If
+import os
+import csv
+
+#set file path
+csvpath = os.path.join('C:/Users/Eric/Downloads/python-challenge/PyBank/resources/budget_data.csv')
+
+#open file
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    
+    #running total number of months 
+    months_total = int(0)
+    #running count of net total amount of "Profit/Losses"
+    net_prof = int(0)
+    #greatest profit tracker value
+    g_profit = float(0)
+    #greatest loss tracker value
+    g_loss = float(0)
+    #count number of changes
+    count = int(0)
+    for row in csvreader:
+        #get current profit/loss value
+        currentval = float(row[1])
+        #increase number of months
+        months_total += 1
+        #add change to net proft loss count
+        net_prof += currentval
+
+        #check if current value is a profit or a loss
+        if currentval >= 0:
+            #check if current profit is greater than greatest profit
+            if currentval > g_profit:
+                #update value and save the date
+                g_profit = currentval
+                g_p_date = row[0]
+        else:
+            #check if current loss is greater than greatest loss
+            if currentval < g_loss:
+                #update value and save the date
+                g_loss = currentval
+                g_l_date = row[0]
+        
+        count +=1
+    #calculate average change
+    avgchange = net_prof/months_total
+
+#export results
+
+output_path = os.path.join('C:/Users/Eric/Downloads/python-challenge/PyBank/analysis/output.txt')
+
+with open(output_path, 'w') as f:
+    f.write('Financial Analysis\n--------------\n')
+    f.write('Total Months: ' + str(months_total) + '\n')
+    f.write('Total: $' + str(net_prof) + '\n')
+    f.write('Average change: $' + str(avgchange) + '\n')
+    f.write('Greatest increase in profits: ' + str(g_p_date) + ' $' + str(g_profit) + '\n')
+    f.write('Total decrease in profits: ' + str(g_l_date) + ' $' + str(g_loss) + '\n')
---
+++ PyPoll/main.py
@@ -0,0 +1,58 @@
+#starting off initializing and reading in the CSV file using code from activity 3-8
+#initialize
+import os
+import csv
+
+#set file path
+csvpath = os.path.join('C:/Users/Eric/Downloads/python-challenge/PyPoll/resources/election_data.csv')
+
+#open file
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+
+    #running total number of votes cast
+    vote_total = 0
+    #list of canidates
+    canidates = []
+    #dictionary to count votes per canidate
+    canidate_votes = {}
+    for row in csvreader:
+        #increase count of total votes
+        vote_total += 1
+        #add to list of canidates if not already in list
+        if row[2] not in canidates:
+            canidates.append(row[2])
+            #add new canidate to canidate vote dictionary
+            canidate_votes[row[2]]=0
+
+        #increment vote count if canidate is already in list
+        canidate_votes[row[2]] +=1 
+
+#export results
+output_path = os.path.join('C:/Users/Eric/Downloads/python-challenge/PyPoll/analysis/output.txt')
+
+with open(output_path, 'w') as f:
+    f.write('Election Results\n--------------\n')
+    f.write('Total Votes: ' + str(vote_total) + '\n--------------\n')
+
+    #2 variables to store winning canidate and the number of their votes
+    highvotes = 0
+    winner = ""
+
+    for canidate in canidate_votes:
+        #get number of votes from dictionary and calculate percentage votes
+        votes=canidate_votes[canidate]
+        percentage_votes = votes/vote_total*100
+        f.write(canidate + ": " + str(percentage_votes) + "% (" + str(votes) + ")\n")
+
+        #determine if canidate has highest number of votes
+        if votes > highvotes:
+            highvotes = votes
+            winner = canidate
+
+    f.write('--------------\nWinner: ' + str(winner) + '\n--------------')
